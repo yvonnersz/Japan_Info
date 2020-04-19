73d9f7750d13cf5067a624287098d8f964c84cf9
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-19 22:40:32 +0000

    add relevant urls needed

--- lib/japan-info/scraper.rb
+++ lib/japan-info/scraper.rb
@@ -3,11 +3,12 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  doc = Nokogiri::HTML(open(https://www.japan-guide.com/e/e623.html))
 
-#interests:
-#interest subcategory:
-#heading:
-#list:
-#info:
+  #interests: doc.css("div.interests_top_page__category_title")
+  #interest subcategory: doc.css("div.link_gallery__link__label")
+  #heading: doc.css("div.page_section__body")
+  #list:doc.css("spot_list__spot__main_info h1")
+  #info: doc.css("div.spot_list__spot__desc")
 
 end
