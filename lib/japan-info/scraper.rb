cca0c30c657be0e3556447767f50673550192643
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-19 22:45:32 +0000

    add permission and binding.pry

--- lib/japan-info/scraper.rb
+++ lib/japan-info/scraper.rb
@@ -3,12 +3,16 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  doc = Nokogiri::HTML(open(https://www.japan-guide.com/e/e623.html))
 
-  #interests: doc.css("div.interests_top_page__category_title")
-  #interest subcategory: doc.css("div.link_gallery__link__label")
-  #heading: doc.css("div.page_section__body")
-  #list:doc.css("spot_list__spot__main_info h1")
-  #info: doc.css("div.spot_list__spot__desc")
+  def self.scrape
+    doc = Nokogiri::HTML(open(https://www.japan-guide.com/e/e623.html))
+
+    #interests: doc.css("div.interests_top_page__category_title")
+    #interest subcategory: doc.css("div.link_gallery__link__label")
+    #heading: doc.css("div.page_section__body")
+    #list:doc.css("spot_list__spot__main_info h1")
+    #info: doc.css("div.spot_list__spot__desc")
+    binding.pry
+  end
 
 end
