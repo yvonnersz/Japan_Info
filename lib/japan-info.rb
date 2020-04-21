6389d3fe797a14c3cc576ecb90a3c058b81a0071
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-21 13:25:01 +0000

    subinterests options now work

--- lib/japan-info.rb
+++ lib/japan-info.rb
@@ -10,18 +10,22 @@ require 'pry'
 #heading: doc.css("div.page_section__body")
 #list:doc.css("spot_list__spot__main_info h1")
 #info: doc.css("div.spot_list__spot__desc")
+#link to next site: doc.css("div.link_gallery__links a").attribute("href").value
 
 module JapanInfo
   class Japan
 
-    def initialize(interests)
+    def initialize(interests, subinterests)
       @interests = interests
       @interests = []
+      @subinterests = sub_interests
+      @subinterests = []
     end
 
     def self.load
       doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e623.html"))
       text = doc.css("div.interests_top_page__category_title")
+
 
       new_text = text.collect do |interest|
         interest.text
@@ -36,6 +40,28 @@ module JapanInfo
       @interests.each do |interest|
         puts "#{counter}. #{interest}"
         counter +=1
+      end
+    end
+
+
+
+    def self.sub_interest
+      doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e623.html"))
+      binding.pry
+      subinterests = doc.css("div.link_gallery__link__label")
+
+      subinterest_array = subinterests.collect do |subinterest|
+        subinterest.text
+      end
+
+      @subinterests = subinterest_array[0..5]
+    end
+
+    def self.subinterests_reader
+      counter = 1
+        @subinterests.each do |subinterest|
+          puts "#{counter}. #{subinterest}"
+          counter +=1
       end
     end
 
--- lib/japan/cli.rb
+++ lib/japan/cli.rb
@@ -17,20 +17,24 @@ module JapanInfo
 
       choice = gets.strip
       case choice
-      when 1
-        hiking
-      when 2
+      when "1"
+        sub_interest
+      when "2"
         onsen
-      when 3
+      when "3"
         puts "Goodbye"
         exit
       end
 
     end
 
-    def hiking
-      puts "you chose hiking"
-      main_menu
+    def sub_interest
+      JapanInfo::Japan.sub_interest
+      puts " "
+      puts "You chose 1. What would you like to see more info on?"
+      JapanInfo::Japan.subinterests_reader
+      choice = gets.strip
+
     end
 
     def onsen
