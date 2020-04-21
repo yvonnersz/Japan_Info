28ca249d6ad819474007d848ecadb541a4876088
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-21 14:23:08 +0000

    add onsens

--- lib/japan-info.rb
+++ lib/japan-info.rb
@@ -12,58 +12,118 @@ require 'pry'
 #info: doc.css("div.spot_list__spot__desc")
 #link to next site: doc.css("div.link_gallery__links a").attribute("href").value
 
+# module JapanInfo
+#   class Japan
+#
+#     def initialize(interests, subinterests)
+#       @interests = interests
+#       @interests = []
+#       @subinterests = sub_interests
+#       @subinterests = []
+#     end
+#
+#     def self.load
+#       doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e623.html"))
+#       text = doc.css("div.interests_top_page__category_title")
+#
+#
+#       new_text = text.collect do |interest|
+#         interest.text
+#       end
+#
+#       @interests = new_text
+#
+#     end
+#
+#     def self.reader_interests
+#       counter = 1
+#       @interests.each do |interest|
+#         puts "#{counter}. #{interest}"
+#         counter +=1
+#       end
+#     end
+#
+#
+#
+#     def self.sub_interest
+#       doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e623.html"))
+#       subinterests = doc.css("div.link_gallery__link__label")
+#
+#       subinterest_array = subinterests.collect do |subinterest|
+#         subinterest.text
+#       end
+#
+#       @subinterests = subinterest_array[0..5]
+#     end
+#
+#     def self.subinterests_reader
+#       counter = 1
+#         @subinterests.each do |subinterest|
+#           puts "#{counter}. #{subinterest}"
+#           counter +=1
+#       end
+#     end
+#
+#   end
+# end
+
+
 module JapanInfo
   class Japan
 
-    def initialize(interests, subinterests)
-      @interests = interests
-      @interests = []
-      @subinterests = sub_interests
-      @subinterests = []
-    end
+    def self.onsen
+      doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e2292.html"))
+      region = doc.css(".page_section__body h3")
 
-    def self.load
-      doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e623.html"))
-      text = doc.css("div.interests_top_page__category_title")
-
-
-      new_text = text.collect do |interest|
-        interest.text
+      @region_array = region.collect do |region|
+        region.text
       end
-
-      @interests = new_text
-
     end
 
-    def self.reader_interests
+    def self.onsen_reader
       counter = 1
-      @interests.each do |interest|
-        puts "#{counter}. #{interest}"
-        counter +=1
-      end
-    end
-
-
-
-    def self.sub_interest
-      doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e623.html"))
-      binding.pry
-      subinterests = doc.css("div.link_gallery__link__label")
-
-      subinterest_array = subinterests.collect do |subinterest|
-        subinterest.text
-      end
-
-      @subinterests = subinterest_array[0..5]
-    end
-
-    def self.subinterests_reader
-      counter = 1
-        @subinterests.each do |subinterest|
-          puts "#{counter}. #{subinterest}"
+        @region_array[0..6].each do |region|
+          puts "#{counter}. #{region}"
           counter +=1
       end
+
+      def self.kanto
+        doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e2292.html"))
+        kanto = doc.css(".spot_list__spot__name")
+
+        @kanto_array = kanto.collect do |kanto|
+          kanto.text
+        end
+      end
+
+      def self.kanto_reader
+        counter = 1
+          @kanto_array[0..9].each do |region|
+            puts "#{counter}. #{region}"
+            counter +=1
+        end
+      end
+
+      def self.kanto_spots
+        doc = Nokogiri::HTML(open("https://www.japan-guide.com/e/e7402.html"))
+        kanto_spots = doc.css(".spot_list__spot__name")
+
+        @kanto_spots_array = kanto_spots.collect do |kanto_spot|
+          kanto_spot.text
+        end
+      end
+
+      def self.kanto_spots_reader
+        counter = 1
+          @kanto_spots_array.each do |spot|
+            puts "#{counter}. #{spot}"
+            counter +=1
+        end
+      end
+
+
     end
+
 
   end
 end
--- lib/japan/cli.rb
+++ lib/japan/cli.rb
@@ -6,40 +6,74 @@ module JapanInfo
     end
 
 
-    def main_menu
-      JapanInfo::Japan.load
-      puts "Greetings! Welcome to JapanInfo."
-      puts " "
-      sleep 1.0
-      JapanInfo::Japan.reader_interests
-      puts " "
-      puts "Please input the number you are interested in."
+#     def main_menu
+#       JapanInfo::Japan.load
+#       puts "Greetings! Welcome to JapanInfo."
+#       puts " "
+#       sleep 1.0
+#       JapanInfo::Japan.reader_interests
+#       puts " "
+#       puts "Please input the number you are interested in."
+#
+#       choice = gets.strip
+#       case choice
+#       when "1"
+#         sub_interest
+#       when "2"
+#         onsen
+#       when "3"
+#         puts "Goodbye"
+#         exit
+#       end
+#
+#     end
+#
+#     def sub_interest
+#       JapanInfo::Japan.sub_interest
+#       puts " "
+#       puts "You chose 1. What would you like to see more info on?"
+#       JapanInfo::Japan.subinterests_reader
+#       choice = gets.strip
+#
+#     end
+#
+#     def onsen
+#       puts "you chose onsen"
+#       main_menu
+#     end
+#   end
+# end
 
-      choice = gets.strip
-      case choice
-      when "1"
-        sub_interest
-      when "2"
-        onsen
-      when "3"
-        puts "Goodbye"
-        exit
-      end
-
-    end
-
-    def sub_interest
-      JapanInfo::Japan.sub_interest
-      puts " "
-      puts "You chose 1. What would you like to see more info on?"
-      JapanInfo::Japan.subinterests_reader
-      choice = gets.strip
-
-    end
-
-    def onsen
-      puts "you chose onsen"
-      main_menu
+  def main_menu
+    JapanInfo::Japan.onsen
+    puts "What region are you interested in?"
+    JapanInfo::Japan.onsen_reader
+    choice = gets.strip
+    case choice
+    when "1"
+      first_choice
+    when "2"
+      hokkaido
     end
   end
+
+  def first_choice
+    JapanInfo::Japan.kanto
+    puts "Which city are you interested in?"
+    JapanInfo::Japan.kanto_reader
+    choice = gets.strip
+    case choice
+    when "1"
+      kanto_spots
+    end
+
+  end
+
+  def kanto_spots
+    JapanInfo::Japan.kanto_spots
+    puts "You chose this. Which onsen would you like more info on?"
+    JapanInfo::Japan.kanto_spots_reader
+  end
+
+end
 end
