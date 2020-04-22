8b2aed96a3bbb671f6929774e58fa8a5f5249df6
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-22 05:44:59 +0000

    forced code to be more abstract

--- lib/japan/cli.rb
+++ lib/japan/cli.rb
@@ -62,23 +62,7 @@ module JapanInfo
       puts "You chose #{choice}."
       JapanInfo::Japan.kusatsu_nearby_schedule
       JapanInfo::Japan.kusatsu_nearby_schedule_reader(choice.to_i-1)
-    # when "2"
-    #   JapanInfo::Japan.kusatsu_nearby_schedule
-    #   JapanInfo::Japan.kusatsu_nearby_schedule_reader(1)
-    # when "3"
-    #   JapanInfo::Japan.kusatsu_nearby_schedule
-    #   JapanInfo::Japan.kusatsu_nearby_schedule_reader(2)
-    # when "4"
-    #   JapanInfo::Japan.kusatsu_nearby_schedule
-    #   JapanInfo::Japan.kusatsu_nearby_schedule_reader(3)
-    # when "5"
-    #   JapanInfo::Japan.kusatsu_nearby_schedule
-    #   JapanInfo::Japan.kusatsu_nearby_schedule_reader(4)
-    # when "6"
-    #   JapanInfo::Japan.kusatsu_nearby_schedule
-    #   JapanInfo::Japan.kusatsu_nearby_schedule_reader(5)
-    # end
-  end
+    end
   end
 
 
@@ -98,25 +82,10 @@ module JapanInfo
 
     choice = gets.strip
     case choice
-    when "1"
-      puts "You chose 1."
+    when "#{choice}"
+      puts "You chose #{choice}."
       JapanInfo::Japan.manza_nearby_schedule
-      JapanInfo::Japan.manza_nearby_schedule_reader(0)
-    when "2"
-      JapanInfo::Japan.manza_nearby_schedule
-      JapanInfo::Japan.manza_nearby_schedule_reader(1)
-    when "3"
-      JapanInfo::Japan.manza_nearby_schedule
-      JapanInfo::Japan.manza_nearby_schedule_reader(2)
-    when "4"
-      JapanInfo::Japan.manza_nearby_schedule
-      JapanInfo::Japan.manza_nearby_schedule_reader(3)
-    when "5"
-      JapanInfo::Japan.manza_nearby_schedule
-      JapanInfo::Japan.manza_nearby_schedule_reader(4)
-    when "6"
-      JapanInfo::Japan.manza_nearby_schedule
-      JapanInfo::Japan.manza_nearby_schedule_reader(5)
+      JapanInfo::Japan.manza_nearby_schedule_reader(choice.to_i-1)
     end
   end
 
@@ -137,48 +106,16 @@ module JapanInfo
 
     choice = gets.strip
     case choice
-    when "1"
-      puts "You chose 1."
+    when "#{choice}"
+      puts "You chose #{choice}."
       JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(0)
-    when "2"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(1)
-    when "3"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(2)
-    when "4"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(3)
-    when "5"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(4)
-    when "6"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "7"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "8"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "9"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "10"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "11"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "12"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
-    when "13"
-      JapanInfo::Japan.hakone_nearby_schedule
-      JapanInfo::Japan.hakone_nearby_schedule_reader(5)
+      JapanInfo::Japan.hakone_nearby_schedule_reader(choice.to_i-1)
     end
   end
+
+
+
+
 
   def minakami
     puts ""
@@ -194,24 +131,16 @@ module JapanInfo
 
     choice = gets.strip
     case choice
-    when "1"
-      puts "You chose 1."
+    when "#{choice}"
+      puts "You chose #{choice}."
       JapanInfo::Japan.minakami_nearby_schedule
-      JapanInfo::Japan.minakami_nearby_schedule_reader(0)
-    when "2"
-      JapanInfo::Japan.minakami_nearby_schedule
-      JapanInfo::Japan.minakami_nearby_schedule_reader(1)
-    when "3"
-      JapanInfo::Japan.minakami_nearby_schedule
-      JapanInfo::Japan.minakami_nearby_schedule_reader(2)
-    when "4"
-      JapanInfo::Japan.minakami_nearby_schedule
-      JapanInfo::Japan.minakami_nearby_schedule_reader(3)
-    when "5"
-      JapanInfo::Japan.minakami_nearby_schedule
-      JapanInfo::Japan.minakami_nearby_schedule_reader(4)
+      JapanInfo::Japan.minakami_nearby_schedule_reader(choice.to_i-1)
     end
   end
+
+
+
+
 
   def nasu
     puts ""
@@ -227,264 +156,130 @@ module JapanInfo
 
     choice = gets.strip
     case choice
-    when "1"
-      puts "You chose 1."
+    when "#{choice}"
+      puts "You chose #{choice}."
       JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(0)
-    when "2"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(1)
-    when "3"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(2)
-    when "4"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(3)
-    when "5"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(4)
-    when "6"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(5)
-    when "7"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(6)
-    when "8"
-      JapanInfo::Japan.nasu_nearby_schedule
-      JapanInfo::Japan.nasu_nearby_schedule_reader(7)
+      JapanInfo::Japan.nasu_nearby_schedule_reader(choice.to_i-1)
     end
-end
-
-def nikko
-  puts ""
-  puts "You chose 6."
-  puts ""
-  sleep 2
-  JapanInfo::Japan.nikko_info
-  JapanInfo::Japan.nikko_info_reader
-  puts ""
-  puts "Would you like to explore nearby community bath houses?"
-  JapanInfo::Japan.nikko_spots
-  JapanInfo::Japan.nikko_spots_reader
-
-  choice = gets.strip
-  case choice
-  when "1"
-    puts "You chose 1."
-    JapanInfo::Japan.nikko_nearby_schedule
-    JapanInfo::Japan.nikko_nearby_schedule_reader(0)
-  when "2"
-    puts "You chose 2."
-    JapanInfo::Japan.nikko_nearby_schedule
-    JapanInfo::Japan.nikko_nearby_schedule_reader(1)
-  when "3"
-    puts "You chose 3."
-    JapanInfo::Japan.nikko_nearby_schedule
-    JapanInfo::Japan.nikko_nearby_schedule_reader(2)
   end
-end
 
 
 
-def ikaho
-  puts ""
-  puts "You chose 7."
-  puts ""
-  sleep 2
-  JapanInfo::Japan.ikaho_info
-  JapanInfo::Japan.ikaho_info_reader
-  puts ""
-  puts "Would you like to explore nearby community bath houses?"
-  JapanInfo::Japan.ikaho_spots
-  JapanInfo::Japan.ikaho_spots_reader
 
-  choice = gets.strip
-  case choice
-  when "1"
-    puts "You chose 1."
-    JapanInfo::Japan.ikaho_nearby_schedule
-    JapanInfo::Japan.ikaho_nearby_schedule_reader(0)
-  when "2"
-    puts "You chose 2."
-    JapanInfo::Japan.ikaho_nearby_schedule
-    JapanInfo::Japan.ikaho_nearby_schedule_reader(1)
-  end
-end
-
-
-
-def kinugawa
-  puts ""
-  puts "You chose 8."
-  puts ""
-  sleep 2
-  JapanInfo::Japan.kinugawa_info
-  JapanInfo::Japan.kinugawa_info_reader
-  puts ""
-  puts "Would you like to explore nearby community bath houses?"
-  JapanInfo::Japan.kinugawa_spots
-  JapanInfo::Japan.kinugawa_spots_reader
-
-  choice = gets.strip
-  case choice
-  when "1"
-    puts "You chose 1."
-    JapanInfo::Japan.kinugawa_nearby_schedule
-    JapanInfo::Japan.kinugawa_nearby_schedule_reader(0)
-  when "2"
-    puts "You chose 2."
-    JapanInfo::Japan.kinugawa_nearby_schedule
-    JapanInfo::Japan.kinugawa_nearby_schedule_reader(1)
-  end
-end
-
-
-
-def shiobara
-  puts ""
-  puts "You chose 9."
-  puts ""
-  sleep 2
-  JapanInfo::Japan.shiobara_info
-  JapanInfo::Japan.shiobara_info_reader
-  puts ""
-  puts "Would you like to explore nearby community bath houses?"
-  JapanInfo::Japan.shiobara_spots
-  JapanInfo::Japan.shiobara_spots_reader
-
-  choice = gets.strip
-  case choice
-  when "1"
-    puts "You chose 1."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(0)
-  when "2"
-    puts "You chose 2."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(1)
-  when "3"
-    puts "You chose 3."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(2)
-  when "4"
-    puts "You chose 4."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(3)
-  when "5"
-    puts "You chose 5."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(4)
-  when "6"
+  def nikko
+    puts ""
     puts "You chose 6."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(5)
-  when "7"
-    puts "You chose 7."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(6)
-  when "8"
-    puts "You chose 8."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(7)
-  when "9"
-    puts "You chose 9."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(8)
-  when "10"
-    puts "You chose 10."
-    JapanInfo::Japan.shiobara_nearby_schedule
-    JapanInfo::Japan.shiobara_nearby_schedule_reader(9)
+    puts ""
+    sleep 2
+    JapanInfo::Japan.nikko_info
+    JapanInfo::Japan.nikko_info_reader
+    puts ""
+    puts "Would you like to explore nearby community bath houses?"
+    JapanInfo::Japan.nikko_spots
+    JapanInfo::Japan.nikko_spots_reader
+
+    choice = gets.strip
+    case choice
+    when "#{choice}"
+      puts "You chose #{choice}."
+      JapanInfo::Japan.nikko_nearby_schedule
+      JapanInfo::Japan.nikko_nearby_schedule_reader(choice.to_i-1)
+    end
   end
-end
 
 
 
-
-
-def shima
-  puts ""
-  puts "You chose 10."
-  puts ""
-  sleep 2
-  JapanInfo::Japan.shima_info
-  JapanInfo::Japan.shima_info_reader
-  puts ""
-  puts "Would you like to explore nearby community bath houses?"
-  JapanInfo::Japan.shima_spots
-  JapanInfo::Japan.shima_spots_reader
-
-  choice = gets.strip
-  case choice
-  when "1"
-    puts "You chose 1."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(0)
-  when "2"
-    puts "You chose 2."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(1)
-  when "3"
-    puts "You chose 3."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(2)
-  when "4"
-    puts "You chose 4."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(3)
-  when "5"
-    puts "You chose 5."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(4)
-  when "6"
-    puts "You chose 6."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(5)
-  when "7"
+  def ikaho
+    puts ""
     puts "You chose 7."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(6)
-  when "8"
-    puts "You chose 8."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(7)
-  when "9"
-    puts "You chose 9."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(8)
-  when "10"
-    puts "You chose 10."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(9)
-  when "9"
-    puts "You chose 11."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(11)
-  when "10"
-    puts "You chose 12."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(12)
-  when "9"
-    puts "You chose 13."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(13)
-  when "10"
-    puts "You chose 14."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(14)
-  when "9"
-    puts "You chose 15."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(15)
-  when "10"
-    puts "You chose 16."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(16)
-  when "9"
-    puts "You chose 17."
-    JapanInfo::Japan.shima_nearby_schedule
-    JapanInfo::Japan.shima_nearby_schedule_reader(17)
+    puts ""
+    sleep 2
+    JapanInfo::Japan.ikaho_info
+    JapanInfo::Japan.ikaho_info_reader
+    puts ""
+    puts "Would you like to explore nearby community bath houses?"
+    JapanInfo::Japan.ikaho_spots
+    JapanInfo::Japan.ikaho_spots_reader
+
+    choice = gets.strip
+    case choice
+    when "#{choice}"
+      puts "You chose #{choice}."
+      JapanInfo::Japan.ikaho_nearby_schedule
+      JapanInfo::Japan.ikaho_nearby_schedule_reader(choice.to_i)
+    end
   end
-end
+
+
+
+  def kinugawa
+    puts ""
+    puts "You chose 8."
+    puts ""
+    sleep 2
+    JapanInfo::Japan.kinugawa_info
+    JapanInfo::Japan.kinugawa_info_reader
+    puts ""
+    puts "Would you like to explore nearby community bath houses?"
+    JapanInfo::Japan.kinugawa_spots
+    JapanInfo::Japan.kinugawa_spots_reader
+
+    choice = gets.strip
+    case choice
+    when "#{choice}"
+      puts "You chose #{choice}."
+      JapanInfo::Japan.kinugawa_nearby_schedule
+      JapanInfo::Japan.kinugawa_nearby_schedule_reader(choice.to_i)
+    end
+  end
+
+
+
+  def shiobara
+    puts ""
+    puts "You chose 9."
+    puts ""
+    sleep 2
+    JapanInfo::Japan.shiobara_info
+    JapanInfo::Japan.shiobara_info_reader
+    puts ""
+    puts "Would you like to explore nearby community bath houses?"
+    JapanInfo::Japan.shiobara_spots
+    JapanInfo::Japan.shiobara_spots_reader
+
+    choice = gets.strip
+    case choice
+    when "#{choice}"
+      puts "You chose #{choice}."
+      JapanInfo::Japan.shiobara_nearby_schedule
+      JapanInfo::Japan.shiobara_nearby_schedule_reader(choice.to_i)
+    end
+  end
+
+
+
+
+
+  def shima
+    puts ""
+    puts "You chose 10."
+    puts ""
+    sleep 2
+    JapanInfo::Japan.shima_info
+    JapanInfo::Japan.shima_info_reader
+    puts ""
+    puts "Would you like to explore nearby community bath houses?"
+    JapanInfo::Japan.shima_spots
+    JapanInfo::Japan.shima_spots_reader
+
+    choice = gets.strip
+    case choice
+    when "#{choice}"
+      puts "You chose #{choice}."
+      JapanInfo::Japan.shima_nearby_schedule
+      JapanInfo::Japan.shima_nearby_schedule_reader(choice.to_i-1)
+    end
+  end
 
 end
 end
