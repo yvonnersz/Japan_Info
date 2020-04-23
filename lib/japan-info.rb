dc585b66e68bab2aa4e6f0ee402c48009c1fc87e
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-23 17:35:29 +0000

    delete nonessential code in japan-info.rb

--- lib/japan-info.rb
+++ lib/japan-info.rb
@@ -31,34 +31,6 @@ module JapanInfo
         end
       end
 
-      def self.kanto_spots
-        doc = JapanInfo::Scraper.kusatsu_page
-        kanto_spots = doc.css(".spot_list__spot__name")
-        @kanto_spots_array = kanto_spots.collect do |kanto_spot|
-          kanto_spot.text
-        end
-      end
-
-      def self.kanto_spots_info
-        doc = JapanInfo::Scraper.kusatsu_page
-        kanto_spots_info = doc.css("span.spot_meta__content")
-        @kanto_spots_info_array = kanto_spots_info.collect do |kanto_spot_info|
-          kanto_spot_info.text
-        end
-      end
-
-      def self.kanto_spots_reader
-        counter = 1
-          @kanto_spots_array.each do |spot|
-            puts "#{counter}. #{spot}"
-            counter +=1
-        end
-      end
-
-      def self.kanto_spots_info_reader
-        puts "#{@kanto_spots_info_array[0]}"
-      end
-
       def self.info(website)
         doc = website
         @info = doc.css(".page_section__body p[2]").text
