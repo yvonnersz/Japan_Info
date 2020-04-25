9d7fe5827548e4b2a03d269b6fdf6cda3a7a1d8b
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-25 06:31:19 +0000

    cleaning up JapanInfo::Scraper

--- lib/japan/scraper.rb
+++ lib/japan/scraper.rb
@@ -1,26 +1,19 @@
-
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-
 module JapanInfo
   class Scraper
 
-      def get_page
-        Nokogiri::HTML(open("https://www.japan-guide.com/e/e2292.html"))
-      end
+    def get_page
+      Nokogiri::HTML(open("https://www.japan-guide.com/e/e2292.html"))
+    end
 
-      def scrape_onsens_index
-        self.get_page.css(".spot_list__spot")
+    def make_onsens
+      get_page.css(".spot_list__spot").each do |r|
+        JapanInfo::Japan.new_from_index_page(r)
       end
-
-      def make_onsens
-        scrape_onsens_index.each do |r|
-          JapanInfo::Japan.new_from_index_page(r)
-        end
-      end
-
+    end
 
   end
 end
