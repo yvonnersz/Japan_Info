7c454ec4d4d69b864cffe8a5371c7bad6033b1a7
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-25 03:21:52 +0000

    add object description

--- lib/japan/cli.rb
+++ lib/japan/cli.rb
@@ -5,19 +5,29 @@ module JapanInfo
    def start
      JapanInfo::Scraper.new.make_onsens
      puts "Hi! Welcome to JapanInfo. I will be your tour guide today."
-     puts "Which region are you most interested in?"
+     puts "Which onsen are you most interested in the Kanto region (Tokyo)?"
      puts ""
      kanto_onsens
+
      input = gets.strip
+     index = input.to_i - 1
+     description(index)
+
    end
 
    def kanto_onsens
      counter = 1
      JapanInfo::Japan.all[0..9].each do |onsen|
-       puts "#{counter}.         #{onsen.name}"
+       puts "#{counter}.    #{onsen.name}"
        counter +=1
      end
    end
+
+   def description(index)
+     puts "---------------#{JapanInfo::Japan.all[index].name}---------------------------"
+     puts "#{JapanInfo::Japan.all[index].description}"
+   end
+
 
 
 end
