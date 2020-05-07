0c6d3a24b83b929a81f7be246e4cffb5cabe6138
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-07 11:03:34 +0000

    fixed all breaks

--- lib/japan/cli.rb
+++ lib/japan/cli.rb
@@ -6,9 +6,12 @@ module JapanInfo
      JapanInfo::Scraper.new.make_cities
      puts ""
      puts "Hi! Welcome to JapanInfo. I will be your tour guide today."
+     cities
+   end
+
+   def cities
      puts "Which city in the Kanto region would you like a description on?"
      puts ""
-
      print_cities
      puts ""
      puts "Please input a number."
@@ -37,27 +40,44 @@ module JapanInfo
    def cli_spots
      spots_input = gets.strip
 
-     print_hours(@city,spots_input)
-
+     case spots_input.to_i <= @city.spots.size
+     when false
+       puts ""
+       puts "Please enter a valid number."
+       cli_spots
+     when true
+       print_hours(@city,spots_input)
+       puts "Would you like to explore more or exit?"
+       explore_more_or_exit
+     end
    end
 
-   def print_description(city_input)
-     puts ""
-     puts "-------------#{JapanInfo::Japan.all[city_input.to_i-1].name.gsub(/([•])/,'')}-------------"
-     puts "#{JapanInfo::Japan.all[city_input.to_i-1].description}"
-     puts ""
+   def explore_more_or_exit
+     explore_or_exit_input = gets.strip
+
+     case explore_or_exit_input.downcase
+     when "exit"
+       puts "Goodbye!"
+       exit
+     when "explore", "more"
+       cities
+     else
+       puts "Please enter a valid response."
+       explore_more_or_exit
+     end
    end
+
 
    def print_spots(city)
-     city.spots.each_with_index {|place,i| puts "#{i+1}.  #{place}"}
+     city.spots.collect.with_index(1) {|city,i| puts "#{i}.  #{city}"}
    end
 #
-   def print_hours(city, input)
+   def print_hours(city, spots_input)
      puts ""
-     puts "-------------#{city.spots[input.to_i-1]}-------------"
-     puts "#{city.hours[input.to_i-1]}"
+     puts "-------------#{city.spots[spots_input.to_i-1]}-------------"
+     puts "#{city.hours[spots_input.to_i-1]}"
      puts ""
-     puts "#{city.info[input.to_i-1]}"
+     puts "#{city.info[spots_input.to_i-1]}"
      puts ""
    end
 
@@ -65,6 +85,13 @@ module JapanInfo
      JapanInfo::Japan.all.each_with_index do |city, i|
        puts "#{i+1}.  #{city.name.gsub(/([•])/,'')}"
      end
+   end
+
+   def print_description(city_input)
+     puts ""
+     puts "-------------#{JapanInfo::Japan.all[city_input.to_i-1].name.gsub(/([•])/,'')}-------------"
+     puts "#{JapanInfo::Japan.all[city_input.to_i-1].description}"
+     puts ""
    end
 
  end
