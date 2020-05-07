25ea79f7b3e29cc877d4e10efb0f2bcb9a2ca557
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-07 10:36:01 +0000

    code doesn't break for user input

--- lib/japan/cli.rb
+++ lib/japan/cli.rb
@@ -7,90 +7,51 @@ module JapanInfo
      puts ""
      puts "Hi! Welcome to JapanInfo. I will be your tour guide today."
      puts "Which city in the Kanto region would you like a description on?"
-     puts "Please input a number."
      puts ""
 
      print_cities
-     cities
+     puts ""
+     puts "Please input a number."
+     cli_cities
    end
 
-   def print_cities
-     JapanInfo::Japan.all.each_with_index do |city, i|
-       puts "#{i+1}.  #{city.name.gsub(/([•])/,'')}"
-     end
-   end
-
-   def cities
+   def cli_cities
      city_input = gets.strip
 
-     case city_input.to_i <= print_cities.size
+     case city_input.to_i <= JapanInfo::Japan.all.size
      when false
        puts ""
        puts "Please enter a valid number."
-       puts ""
-       sleep 1
-       print_cities
-       cities
+       cli_cities
      when true
-       print_description(@city_input)
-       spots_in_city
+       print_description(city_input)
+       puts "Here are the nearby onsen spots/hotels."
+       puts "Which would you like more information on?"
+
+       @city = JapanInfo::Japan.find(city_input)
+       print_spots(@city)
+       cli_spots
      end
    end
 
-   def print_description(input)
+   def cli_spots
+     spots_input = gets.strip
+
+     print_hours(@city,spots_input)
+
+   end
+
+   def print_description(city_input)
      puts ""
-     puts "-------------#{JapanInfo::Japan.all[input.to_i-1].name.gsub(/([•])/,'')}-------------"
-     puts "#{JapanInfo::Japan.all[input.to_i-1].description}"
+     puts "-------------#{JapanInfo::Japan.all[city_input.to_i-1].name.gsub(/([•])/,'')}-------------"
+     puts "#{JapanInfo::Japan.all[city_input.to_i-1].description}"
      puts ""
    end
 
-   def spots_in_city
-     puts "Would you like to check out nearby onsens/hotels?"
-     puts "Please enter in Yes or No."
-     nearby_spots_input = gets.strip
-
-     case nearby_spots_input.downcase
-     when "yes", "y"
-       onsen_spots
-     when "no", "n"
-       puts "Goodbye!"
-       exit
-     else
-       puts ""
-       puts "Please enter a valid input."
-       puts ""
-       sleep 1
-       spots_in_city
-     end
-   end
-
-   def onsen_spots
-     city = JapanInfo::Japan.find(city_input)
-     puts ""
-     print_spots(city)
-
-     puts ""
-     puts "Please input the number in which onsen you would like more info on."
-
-     input=gets.strip
-     print_hours(city,input)
-
-     puts ""
-     puts "Would you like to keep browsing? (y/n)"
-     input = gets.strip
-     case input.downcase
-     when "y"
-       start
-     when "n"
-       puts "Goodbye!"
-       exit
-     end
-   end
-
-   def print_spots(city_input)
+   def print_spots(city)
      city.spots.each_with_index {|place,i| puts "#{i+1}.  #{place}"}
    end
-
+#
    def print_hours(city, input)
      puts ""
      puts "-------------#{city.spots[input.to_i-1]}-------------"
@@ -98,6 +59,12 @@ module JapanInfo
      puts ""
      puts "#{city.info[input.to_i-1]}"
      puts ""
+   end
+
+   def print_cities
+     JapanInfo::Japan.all.each_with_index do |city, i|
+       puts "#{i+1}.  #{city.name.gsub(/([•])/,'')}"
+     end
    end
 
  end
